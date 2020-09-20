84c58315abccdff1db8331c238d5d5965c2495ba
Sean Liao <seankhliao@gmail.com>
2020-09-21 00:34:40 +0200

    insecure

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		metric.WithDescription("hits per endpoint"),
 	)
 
-	cc, err := grpc.Dial(s.streamAddr)
+	cc, err := grpc.Dial(s.streamAddr, grpc.WithInsecure())
 	if err != nil {
 		s.log.Error().Err(err).Msg("connect to stream")
 	}
